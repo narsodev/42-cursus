e3b7a80d5899535a4404ab3616560e52fe3885a5
narsodev <narsodev@gmail.com>
2022-06-03 17:52:32 +0200

    Add files

---
+++ gnl/get_next_line_bonus.h
@@ -0,0 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ngonzale <ngonzale@student.42malaga.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/05 23:39:56 by ngonzale          #+#    #+#             */
+/*   Updated: 2022/05/18 11:53:22 by ngonzale         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+char	*get_next_line(int fd);
+
+#endif
