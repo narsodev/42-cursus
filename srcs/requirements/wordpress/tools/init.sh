90658fb8144e9775b509ef9615b871c5c4142b54
Narciso González Calderón <narsodev@gmail.com>
2023-10-26 02:18:02 +0200

    Remove arg

--- srcs/requirements/wordpress/tools/init.sh
+++ srcs/requirements/wordpress/tools/init.sh
@@ -4,7 +4,6 @@ WP_DB_HOST=$1
 WP_DB_NAME=$2
 WP_DB_USER=$3
 WP_DB_PASSWORD=$4
-WP_CONTENT_DIR=$5
 
 echo "
 <?php
@@ -12,6 +11,6 @@ define('DB_NAME', '$WP_DB_NAME');
 define('DB_USER', '$WP_DB_USER');
 define('DB_PASSWORD', '$WP_DB_PASSWORD');
 define('DB_HOST', '$WP_DB_HOST');
-define('WP_CONTENT_DIR', '$WP_CONTENT_DIR');
+define('WP_CONTENT_DIR', '/var/lib/wordpress/wp-content');
 ?>
 " > /etc/wordpress/config-default.php
